ec0bc70dd164caa5d64f194a5040f3f68d80a323
unknown <odiwuorodero8948@gmail.com>
2025-05-07 20:14:18 +0300

    adding 10th quiz

--- Test2/10.basicatoi2/main.go
+++ Test2/10.basicatoi2/main.go
@@ -1,5 +1,4 @@
 package main
-import "fmt"
 
 func BasicAtoi2(s string) int {
 result :=0
@@ -12,10 +11,4 @@ for i:=0;i < len(s);i++{
 	}
 }
 return result
-}
-func main() {
-	fmt.Println(BasicAtoi2("12345"))
-	fmt.Println(BasicAtoi2("0000000012345"))
-	fmt.Println(BasicAtoi2("012 345"))
-	fmt.Println(BasicAtoi2("Hello World!"))
 }
