6165ecf1ddf50139d42608be181b4c22202afe80
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-10-13 12:41:20 +0300

    changes

---
+++ Test1/Quad_project/QuadC.go
@@ -0,0 +1,47 @@
+ package main
+import zone "github.com/01-edu/z01"
+
+func QuadC(x, y int){
+	if x <= 0 || y <= 0 {
+		return
+	}
+	// Top row
+	for i := 1; i <= x; i++ {
+		if i == 1 {
+			zone.PrintRune('A')
+		} else if i == x {
+			zone.PrintRune('C')
+		} else {
+			zone.PrintRune('B')
+		}
+	}
+	zone.PrintRune('\n')
+
+	// Middle rows
+	for row := 1; row <= y-2; row++ {
+		for col := 1; col <= x; col++ {
+			if col == 1 || col == x {
+				zone.PrintRune('B')
+			} else {
+				zone.PrintRune(' ')
+			}
+		}
+		zone.PrintRune('\n')
+	}
+	// Bottom row 
+	if y > 1 {
+		for i := 1; i <= x; i++ {
+			if i == 1 {
+				zone.PrintRune('A')
+			}else if i == x {
+				zone.PrintRune('C')
+			} else {
+				zone.PrintRune('B')
+			}
+		}
+		zone.PrintRune('\n')
+	}
+}
+	func main() {
+	QuadC(5,3)
+	}
