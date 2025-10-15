8e428b772e47ecba861974af487d8eada0184d04
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-10-15 20:06:21 +0300

    changes

--- Test3/5.fibonacci/main.go
+++ Test3/5.fibonacci/main.go
@@ -4,13 +4,15 @@
 if index < 0 {
 	return -1
 }
-
+if index == 0{
+	return 0
+}
 if index == 1 {
-	return index
+	return 1
 }
 return Fibonacci(index-1)+ Fibonacci(index-2)
 }
 func main(){
-	arg1 :=-4
+	arg1 := 4
 	fmt.Println(Fibonacci(arg1))
 }
