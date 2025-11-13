756564c09a2f37d7b935721ce3bb5e29b1bb28ba
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-11-13 19:45:45 +0300

    changes

--- Test4/main.go
+++ Test4/main.go
@@ -1,15 +1,15 @@
-package main
-
-import "github.com/01-edu/z01"
-
-func LastRune(s string) rune {
-	n := []rune(s)
-
-	return n[len(n)-1]
-}
-func main() {
-	z01.PrintRune(LastRune("Hello!"))
-	z01.PrintRune(LastRune("Salut!"))
-	z01.PrintRune(LastRune("Ola!"))
-	z01.PrintRune('\n')
+package main 
+import "fmt"
+func main(){
+	i := 1
+	for {
+		fmt.Println(i)
+		i++
+		if i == 5 {
+			fmt.Println(i)
+			break
+			
+		}
+		
+	}
 }
