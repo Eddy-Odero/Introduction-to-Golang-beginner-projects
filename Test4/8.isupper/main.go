c4ceed0173ecf31bc7e505f640b2c4beffef8d74
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-11-19 20:12:09 +0300

    changes

--- Test4/8.isupper/main.go
+++ Test4/8.isupper/main.go
@@ -2,16 +2,19 @@ package main
 
 import "fmt"
 
+
 func IsUpper(s string) bool {
-	for _, i := range s {
-		if i >= 'A' && i <= 'Z' {
-			return true
+	for _, r := range s {
+		if r >= 'A' && r <= 'Z' {
+		} else {
+			return false
 		}
 	}
-	return false
+	return true
 }
+
 func main() {
 	fmt.Println(IsUpper("HELLO"))
-	fmt.Println(IsUpper("HELLO!"))
+	fmt.Println(IsUpper("Heh!"))
 
 }
