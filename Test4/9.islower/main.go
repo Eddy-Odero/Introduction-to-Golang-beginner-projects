1c8776d3f1a121358fd8d857507ae8a0c91a1f17
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-11-20 20:16:01 +0300

    changes

--- Test4/9.islower/main.go
+++ Test4/9.islower/main.go
@@ -1,1 +1,16 @@
+package main
+import "fmt"
+func IsLower(s string) bool {
+for _,i := range s{
+	if i >= 'a' && i <='z'{
 
+	}else{
+		return false
+	}
+}
+return true
+}
+func main(){
+fmt.Println(IsLower("hello"))
+	fmt.Println(IsLower("hello!"))
+}
