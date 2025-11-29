fe77a9811ea32fef2b617dc363b45903c147bcea
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-11-29 20:43:17 +0300

    changes

--- Test4/14.tolower/main.go
+++ Test4/14.tolower/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func ToLower(s string) string {
-	result := " "
+	var result = " "
 
 	for _, i := range s {
 		if i >= 'A' && i <= 'Z' {
-			result += string(i - 32)
+			result += string(i + 32)
 		} else {
 			result += string(i)
 		
