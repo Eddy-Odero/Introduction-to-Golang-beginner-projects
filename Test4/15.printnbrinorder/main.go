c87e35824f9698a8a8ad5d746d5d0e321933ccde
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-11-30 19:42:45 +0300

    changes

--- Test4/15.printnbrinorder/main.go
+++ Test4/15.printnbrinorder/main.go
@@ -3,8 +3,13 @@ package main
 func PrintNbrInOrder(n int) {
 digits := []int{}
 if n > 0 {
-digits= append(digits, n%10)
+digits = append(digits, n%10)
 n/=10
+}
+for i:= 0;i < len(digits)-1;i++{
+	for j:=0;j < len(digits)-i-1;j++{
+
+	}
 }
 }
 func main(){
