56ee24fb2ece0ec4877f496b85a739a7dc1991ed
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-11-30 19:58:03 +0300

    changes

--- Test4/15.printnbrinorder/main.go
+++ Test4/15.printnbrinorder/main.go
@@ -1,20 +1,30 @@
 package main
 
-func PrintNbrInOrder(n int) {
-digits := []int{}
-if n > 0 {
-digits = append(digits, n%10)
-n/=10
-}
-for i:= 0;i < len(digits)-1;i++{
-	for j:=0;j < len(digits)-i-1;j++{
+import "fmt"
 
+func PrintNbrInOrder(n int) {
+	digits := []int{}
+	for n > 0 {
+		digits = append(digits, n%10)
+		n /= 10
 	}
+
+	for i := 0; i < len(digits)-1; i++ {
+		for j := 0; j < len(digits)-i-1; j++ {
+			if digits[j] > digits[j+1] {
+				digits[j], digits[j+1] = digits[j+1], digits[j]
+			}
+		}
+	}
+	result := 0
+	for _, d := range digits {
+		result = result*10 + d
+	}
+	fmt.Print(result)
 }
-}
-func main(){
-PrintNbrInOrder(321)
-PrintNbrInOrder(0)
-PrintNbrInOrder(321)
+func main() {
+	PrintNbrInOrder(321)
+	PrintNbrInOrder(0)
+	PrintNbrInOrder(321)
 
 }
