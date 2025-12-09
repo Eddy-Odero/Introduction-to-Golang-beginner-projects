b3d30bf02a412ee4fc7310fb24811394b02fc534
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-12-09 21:29:39 +0300

    finished iscapitalize func

--- Test4/17.capitalize/main.go
+++ Test4/17.capitalize/main.go
@@ -1,1 +1,42 @@
-.
+package main
+
+func toUpper(r rune)rune{
+	if r >= 'a' && r <= 'z' {
+		return r-32
+	}
+	return r
+}
+func toLower(r rune)rune{
+	if r >= 'A' && r <= 'Z' {
+		return r +32
+	}
+	return r
+}
+func isAlnum(r rune)bool {
+	return (r>= 'a' && r <= 'z') ||
+	(r >= 'A'&& r <= 'Z') ||
+	(r >= '0' && r <= '9')
+}
+func Capitalize(s string) string {
+runes := []rune(s)
+inword := false
+
+for i ,r := range runes {
+	if isAlnum(r) {
+		if !inword {
+			runes[i] = toUpper(r)
+			inword = true
+		
+	}else{
+		runes[i] = toLower(r)
+	}
+}else {
+	inword = false
+}
+}
+return string(runes)
+}
+
+func main(){
+
+}
