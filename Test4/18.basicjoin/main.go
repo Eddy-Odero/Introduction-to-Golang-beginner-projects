4c35fefd455d3ed42e21f064ca4c55133c9c8c2d
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-12-10 20:14:19 +0300

    finished the basicjoin solution

--- Test4/18.basicjoin/main.go
+++ Test4/18.basicjoin/main.go
@@ -1,1 +1,14 @@
+package main
+import "fmt"
 
+func BasicJoin(elems []string)string{
+	result := ""
+	for _, s :=range elems{
+		result +=s
+	}
+	return result
+}
+func main() {
+	elems := []string{"Hello!", " How", " are", " you?"}
+	fmt.Println(BasicJoin(elems))
+}
