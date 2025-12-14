3dbcdd97d1b878d0f33c299aaa48beee246afaf5
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-12-14 20:04:09 +0300

    changes

--- Test4/21.atoibase/main.go
+++ Test4/21.atoibase/main.go
@@ -1,7 +1,19 @@
 package main
 func AtoiBase(s string, base string) int {
-
+if !isvalidBase(base){
+	return 0
+}
+sign := 1
+if len(s) > 0 && s[0] == '-'{
+	sign = -1
+	s =s[1:]
+}
+baseLen := len(base)
+result := 0
+for  _, r := range s {
+	index := -1
+}
 }
 func main(){
-	
+
 }
