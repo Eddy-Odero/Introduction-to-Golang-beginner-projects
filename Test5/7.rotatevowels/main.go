b37b0a2a4e801e177544fc5ba5b3ef36aa6c28be
EddyOdero <odiwuorodero8948@gmail.com>
2025-12-24 14:53:12 +0300

    changes

--- Test5/7.rotatevowels/main.go
+++ Test5/7.rotatevowels/main.go
@@ -16,30 +16,44 @@ func isVowel(r rune) bool {
 	return false
 }
 
+func mirrorVowels(s string) string {
+	r := []rune(s)
+
+	var vowels []rune
+	for _, ch := range r {
+		if isVowel(ch) {
+			vowels = append(vowels, ch)
+		}
+	}
+	if len(vowels) == 0 {
+		return s
+	}
+	j := len(vowels) - 1
+	for i := 0; i < len(r); i++ {
+		if isVowel(r[i]) {
+			r[i] = vowels[j]
+			j--
+		}
+	}
+
+	return string(r)
+}
+
 func main() {
 	args := os.Args[1:]
+	if len(args) == 0 {
+		z01.PrintRune('\n')
+		return
+	}
 
-	for a, word := range args {
-		r := []rune(word)
+	for i, arg := range args {
+		result := mirrorVowels(arg)
 
-		var vowels []rune
-		for _, ch := range r {
-			if isVowel(ch) {
-				vowels = append(vowels, ch)
-			}
-		}
-		v := len(vowels) - 1
-		for i := 0; i < len(r); i++ {
-			if isVowel(r[i]) {
-				r[i] = vowels[v]
-				v--
-			}
-		}
-		for _, ch := range r {
+		for _, ch := range result {
 			z01.PrintRune(ch)
 		}
-		if a < len(args)-1 {
-			z01.PrintRune('\n')
+		if i < len(args)-1 {
+			z01.PrintRune(' ')
 		}
 	}
 }
