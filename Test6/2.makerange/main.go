a6c4773014beb6bee313b3479b5c5d7d624d500f
Eddy-Odero <odiwuorodero8948@gmail.com>
2025-12-28 20:03:12 +0300

    changes

--- Test6/2.makerange/main.go
+++ Test6/2.makerange/main.go
@@ -1,1 +1,18 @@
-p
+package main
+import "fmt"
+func MakeRange(min, max int) []int {
+
+	if min >= max {
+		return nil
+	}
+	result := make([]int, max-min)
+
+	for i := 0; i < len(result); i++ {
+		result[i] = min + i
+	}
+return result
+}
+func main() {
+	fmt.Println(MakeRange(5, 10))
+	fmt.Println(MakeRange(10, 5))
+}
