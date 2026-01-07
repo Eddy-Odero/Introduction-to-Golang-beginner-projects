174fc446301526c57f4253dbb6e7704252ad6eeb
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-01-07 22:40:15 +0300

    finished checkpoint 2

--- Test6/checkpoint_exam/2.countalpha/main.go
+++ Test6/checkpoint_exam/2.countalpha/main.go
@@ -1,1 +1,17 @@
-.
+package main
+import "fmt"
+func CountAlpha(s string) int {
+	count := 0
+	for _, i := range s {
+		if i >= 'A' && i <= 'Z' || i >= 'a' && i <= 'z' {
+			count ++
+		}
+	}
+return count
+}
+ 
+func main() {
+	fmt.Println(CountAlpha("Hello world"))
+	fmt.Println(CountAlpha("H e l l o"))
+	fmt.Println(CountAlpha("H1e2l3l4o"))
+}
