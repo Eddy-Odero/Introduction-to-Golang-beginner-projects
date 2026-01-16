a6ff6d33336aac6d8ec683088f12f326fb415805
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-01-16 20:31:19 +0300

    completed quiz

--- Test7/3.displayfile/main.go
+++ Test7/3.displayfile/main.go
@@ -1,1 +1,23 @@
-.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+	if len(os.Args) < 2 {
+		fmt.Println("File name missing")
+		return
+	}
+	  if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		return
+	  }
+	value, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("File name missing")
+		return
+	}
+	fmt.Println(string(value))
+}
