a437f910f58d40eb715e87e9ac411d8191828700
EddyOdero <odiwuorodero8948@gmail.com>
2026-01-20 14:51:43 +0300

    finished foreach

--- Test8/1.foreach/main.go
+++ Test8/1.foreach/main.go
@@ -1,1 +1,25 @@
-.
+package main
+import "github.com/01-edu/z01"
+
+func printNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	if n >= 10 {
+		printNbr(n / 10)
+	}
+	z01.PrintRune(rune(n%10 + '0'))
+}
+
+func ForEach(f func(int), a []int){ 
+for i := 0;i < len(a);i++ {
+f(a[i])
+}
+
+}
+
+func main(){
+a:= []int{1, 2, 3, 4, 5, 6}
+ForEach(printNbr, a)
+}
