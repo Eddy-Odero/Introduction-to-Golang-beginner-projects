19b81a930fd45e91ebac2e9652540390f6a54c54
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-01-22 22:10:39 +0300

    finished doop

--- Test8/6.doop/main.go
+++ Test8/6.doop/main.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -5,73 +6,91 @@ import (
 	"os"
 )
 
-func Atoi(s string) int {
-	if s == " "{
-		return 0
+func atoi(s string) (int64, bool) {
+	if s == "" {
+		return 0, false
 	}
-	result := 0
-    sign := 1
-    i := 0
 
-    if s[0] == '-' {
-        sign = -1
-        i++
-    }else if s[0] =='+'{
+	sign := int64(1)
+	i := 0
+
+	if s[0] == '-' {
+		sign = -1
+		i++
+	} else if s[0] == '+' {
 		i++
 	}
 
-    for j :=i ; j < len(s);j++ {
-        if s[j] < '0' || s[j] > '9' {
-			return 0
-            
-        }
-        result = result*10 + int(s[j]-'0')
-    }
-
-    return result * sign
-}
-
-func add(a, b int) int{
-	return (a+b)
-}
-func sub(a,b int)int{
-	return a-b
-}
-func mult(a, b int)int{
-	return a*b
-}
-func div(a,b int)int{
-	
-	return a/b
-}
-func mod(a,b int)int{
-	
-	return a%b
-}
-
-func main(){
-	if len(os.Args) < 4 {
-		return 
+	if i == len(s) {
+		return 0, false
 	}
-	a:= Atoi(os.Args[1])
-	b:= os.Args[2]
-	c:=Atoi(os.Args[3])
-	
 
-	var op func(int, int) int
-	switch b {
+	var n int64
+	for ; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			return 0, false
+		}
+		d := int64(s[i] - '0')
+
+		// overflow check
+		if n > (1<<63-1-d)/10 {
+			return 0, false
+		}
+		n = n*10 + d
+	}
+
+	n *= sign
+
+	if n > (1<<63-1) || n < -(1<<63) {
+		return 0, false
+	}
+
+	return n, true
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		return
+	}
+
+	a, ok1 := atoi(os.Args[1])
+	op := os.Args[2]
+	b, ok2 := atoi(os.Args[3])
+
+	if !ok1 || !ok2 {
+		return
+	}
+
+	if op == "/" && b == 0 {
+		fmt.Println("No division by 0")
+		return
+	}
+
+	if op == "%" && b == 0 {
+		fmt.Println("No modulo by 0")
+		return
+	}
+
+	var res int64
+	switch op {
 	case "+":
-		op = add
+		res = a + b
 	case "-":
-		op = sub
+		res = a - b
 	case "*":
-		op = mult
+		res = a * b
 	case "/":
-		op = div
+		res = a / b
 	case "%":
-		op = mod
+		res = a % b
 	default:
 		return
 	}
-fmt.Println(op(a,c))
+
+	// overflow check after operation
+	if res > (1<<63-1) || res < -(1<<63) {
+		return
+	}
+
+	fmt.Println(res)
 }
