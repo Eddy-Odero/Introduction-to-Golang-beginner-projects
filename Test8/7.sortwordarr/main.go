d9b850cd386855299b5333255429a9569ba395f4
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-01-23 20:21:34 +0300

    finished sortwordarr

--- Test8/7.sortwordarr/main.go
+++ Test8/7.sortwordarr/main.go
@@ -1,1 +1,23 @@
-.
+package main
+
+import (
+	"fmt"
+	
+)
+func SortWordArr(a []string) {
+	i := len(a)
+	for j:=0;j < i-1;j++{
+		for k :=0;k < i-j-1;k++{
+			if a[k] > a[k+1]{
+				a[k],a[k+1] = a[k+1], a[k]
+			}
+		}
+	}
+
+}
+
+func main() {
+	result := []string{"a", "A", "1", "b", "B", "2", "c", "C", "3"}
+	SortWordArr(result)
+fmt.Println(result)
+}
