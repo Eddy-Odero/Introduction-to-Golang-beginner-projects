b665bd208848b0596dc22f8d8f289dda490dd1ee
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-01-25 20:58:40 +0300

    changes

--- Test9.Hackathon/1.rot14/main.go
+++ Test9.Hackathon/1.rot14/main.go
@@ -6,21 +6,18 @@ import (
 	"github.com/01-edu/z01"
 )
 func Rot14(s string) string {
-result := []rune {}
-for _, i := range s{
-	if i >= 'a' && i <='z'{
-		shift := ((i-'a' +14)%26) + 'a'
-		result = append(result, shift)
+	result := []rune(s)
 
-	}else if i >= 'A' && i <= 'Z'{
-		shift := ((i-'A' + 14 )%26)+'A'
-		result = append(result, shift)
-	}else{
-		result = append(result, i)
+	for i, c := range result {
+		if c >= 'a' && c <= 'z' {
+			result[i] = ((c-'a'+14)%26) + 'a'
+		} else if c >= 'A' && c <= 'Z' {
+			result[i] =  ((c-'A'+14)%26)+ 'A' 
+		}
 	}
+	return string(result)
 }
-return string(result)
-}
+
 
 func main() {
 	result := Rot14("Hello! How are You?")
