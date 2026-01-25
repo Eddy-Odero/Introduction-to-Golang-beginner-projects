69cac4c18605df836b63ef545b822fee2f614989
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-01-25 20:38:07 +0300

    finishe rot14

--- Test9.Hackathon/1.rot14/main.go
+++ Test9.Hackathon/1.rot14/main.go
@@ -6,7 +6,20 @@ import (
 	"github.com/01-edu/z01"
 )
 func Rot14(s string) string {
+result := []rune {}
+for _, i := range s{
+	if i >= 'a' && i <='z'{
+		shift := ((i-'a' +14)%26) + 'a'
+		result = append(result, shift)
 
+	}else if i >= 'A' && i <= 'Z'{
+		shift := ((i-'A' + 14 )%26)+'A'
+		result = append(result, shift)
+	}else{
+		result = append(result, i)
+	}
+}
+return string(result)
 }
 
 func main() {
