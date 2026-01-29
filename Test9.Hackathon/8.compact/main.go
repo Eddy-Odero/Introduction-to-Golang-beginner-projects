70fa6a12e4b94301c4c0636547bb930591cb02b2
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-01-29 20:11:08 +0300

    finished compact

--- Test9.Hackathon/8.compact/main.go
+++ Test9.Hackathon/8.compact/main.go
@@ -1,1 +1,36 @@
-.
+package main
+
+import (
+	"fmt"
+
+)
+func Compact(ptr *[]string) int {
+var result []string
+for _, i := range *ptr {
+	if i != ""{
+result = append(result, i)
+
+	}
+
+}
+*ptr = result
+return len(result)
+}
+const N = 6
+
+func main() {
+	a := make([]string, N)
+	a[0] = "a"
+	a[2] = "b"
+	a[4] = "c"
+
+	for _, v := range a {
+		fmt.Println(v)
+	}
+
+	fmt.Println("Size after compacting:", Compact(&a))
+
+	for _, v := range a {
+		fmt.Println(v)
+	}
+}
