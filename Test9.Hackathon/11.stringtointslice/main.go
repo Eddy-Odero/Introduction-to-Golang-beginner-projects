a2611904e94d2639e3bb74b37963944dc9c7c324
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-01-30 20:26:41 +0300

    finished stringtointslice

--- Test9.Hackathon/11.stringtointslice/main.go
+++ Test9.Hackathon/11.stringtointslice/main.go
@@ -1,1 +1,19 @@
-.
+package main
+
+import (
+	"fmt"
+
+)
+func StringToIntSlice(str string) []int {
+	var result [] int
+for _, i := range str{
+result = append(result, int(i))
+}
+return result
+}
+
+func main() {
+	fmt.Println(StringToIntSlice("A quick brown fox jumps over the lazy dog"))
+	fmt.Println(StringToIntSlice("Converted this string into an int"))
+	fmt.Println(StringToIntSlice("hello THERE"))
+}
