e1f27fa31f28cdac27501a797a6ef866c6c56598
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-02-08 19:34:41 +0300

    finished pushback

--- Test10/1.listpushback/main.go
+++ Test10/1.listpushback/main.go
@@ -1,2 +1,36 @@
 package main
+import "fmt"
 
+type nodeL struct{
+	Data interface{}
+	Next *nodeL
+}
+type List struct{
+	Head *nodeL
+	Tail *nodeL
+}
+func ListPushBack(l *List, data interface{}) {
+n := &nodeL{Data:data}
+ 
+if l.Head == nil {
+	l.Head = n
+	l.Tail = n
+	return 
+
+}
+l.Tail.Next = n
+l.Tail = n
+}
+func main() {
+
+	link := &List{}
+
+	ListPushBack(link, "Hello")
+	ListPushBack(link, "man")
+ 	ListPushBack(link, "how are you")
+
+	for link.Head != nil {
+		fmt.Println(link.Head.Data)
+		link.Head = link.Head.Next
+	}
+}
