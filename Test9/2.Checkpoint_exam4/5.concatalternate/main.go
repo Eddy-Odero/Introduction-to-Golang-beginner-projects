1251d0247dd1cc15a4114ef7e5e03115e52b44e5
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-02-09 21:22:57 +0300

    finished concataltenate

--- Test9/2.Checkpoint_exam4/5.concatalternate/main.go
+++ Test9/2.Checkpoint_exam4/5.concatalternate/main.go
@@ -1,1 +1,31 @@
-.
+package main 
+import "fmt"
+
+func ConcatAlternate(slice1,slice2 []int) []int {
+	var first,second  []int
+
+	if len(slice2) > len(slice1){
+		first = slice2
+		second = slice1
+	}else{
+	first = slice1
+	second = slice2
+
+}
+var result []int
+for i := 0; i < len(first) || i < len(second); i++{
+	if i < len(first){
+		result = append(result, first[i])
+	}
+	if i < len(second){
+		result = append(result, second[i])
+	}
+}
+return result
+}
+func main() {
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{}))
+}
