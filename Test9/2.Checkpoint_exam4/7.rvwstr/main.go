133b8244a0735641e452d80f4f0a929d059f0f07
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-02-11 22:15:56 +0300

    finishe rvwstr

--- Test9/2.Checkpoint_exam4/7.rvwstr/main.go
+++ Test9/2.Checkpoint_exam4/7.rvwstr/main.go
@@ -1,1 +1,29 @@
-.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		return
+	}
+
+	s := os.Args[1]
+	n := len(s)
+
+	end := n
+
+	for i := n - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			fmt.Print(s[i+1 : end])
+			fmt.Print(" ")
+			end = i
+		}
+	}
+
+	
+	fmt.Print(s[0:end])
+	fmt.Println()
+}
