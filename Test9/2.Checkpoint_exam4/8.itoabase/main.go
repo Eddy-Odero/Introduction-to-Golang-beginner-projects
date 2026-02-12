f42fbb96e8ca2a3fc7208438a1c22f26b5e2efc5
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-02-12 20:06:40 +0300

    finished itoabase

--- Test9/2.Checkpoint_exam4/8.itoabase/main.go
+++ Test9/2.Checkpoint_exam4/8.itoabase/main.go
@@ -1,1 +1,36 @@
 package main
+func ItoaBase(value, base int) string {
+	if value == 0 {
+		return "0"
+	}
+
+	digits := "0123456789ABCDEF"
+	negative := false
+
+	if value < 0 {
+		negative = true
+		value = -value
+	}
+
+	result := ""
+
+	for value > 0 {
+		remainder := value % base
+		result = string(digits[remainder]) + result
+		value /= base
+	}
+
+	if negative {
+		result = "-" + result
+	}
+
+	return result
+}
+
+func main(){
+	ItoaBase(42, 2) 
+ItoaBase(42, 16)  
+ItoaBase(-42, 16) 
+ItoaBase(255, 16) 
+
+}
