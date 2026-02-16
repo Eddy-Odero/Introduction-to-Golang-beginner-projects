8be24aaa0de0d86f8e52b3206c3c1761f8acc0a7
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-02-16 20:50:01 +0300

    finished lastlist

--- Test10/4.listlast/main.go
+++ Test10/4.listlast/main.go
@@ -1,1 +1,45 @@
-.
+package main
+
+import (
+	"fmt"
+)
+
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+func ListPushBack(l *List, data interface{}) {
+n := &NodeL{Data:data}
+ 
+if l.Head == nil {
+	l.Head = n
+	l.Tail = n
+	return 
+
+}
+l.Tail.Next = n
+l.Tail = n
+}
+func ListLast(l *List) interface{} {
+if l == nil || l.Tail == nil {
+		return nil
+	}
+	return l.Tail.Data
+}
+func main() {
+	link := &List{}
+	link2 := &List{}
+
+	ListPushBack(link, "three")
+	ListPushBack(link, 3)
+	ListPushBack(link, "1")
+
+	fmt.Println(ListLast(link))
+	fmt.Println(ListLast(link2))
+}
