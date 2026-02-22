31373899c51e3e3e23b85a89ed530b4880c22b32
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-02-22 20:08:25 +0300

    updates

---
+++ Test10/14.sortlistinsert/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+
+	
+)
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+func PrintList(l *NodeI) {
+	it := l
+	for it != nil {
+		fmt.Print(it.Data, " -> ")
+		it = it.Next
+	}
+	fmt.Print(nil, "\n")
+}
+
+func listPushBack(l *NodeI, data int) *NodeI {
+	n := &NodeI{Data: data}
+
+	if l == nil {
+		return n
+	}
+	iterator := l
+	for iterator.Next != nil {
+		iterator = iterator.Next
+	}
+	iterator.Next = n
+	return l
+}
+func SortListInsert(l *NodeI, data_ref int) *NodeI {
+	newNode := &NodeI{Data: data_ref}
+
+	if l == nil {
+		return newNode
+	}
+
+	if data_ref < l.Data {
+		newNode.Next = l
+		return newNode
+	}
+	current := l
+	for current.Next != nil && current.Next.Data < data_ref {
+		current = current.Next
+	}
+
+	newNode.Next = current.Next
+	current.Next = newNode
+
+	return l
+}
+
+func main() {
+
+	var link *NodeI
+
+	link = listPushBack(link, 1)
+	link = listPushBack(link, 4)
+	link = listPushBack(link, 9)
+
+	PrintList(link)
+
+	link = SortListInsert(link, -2)
+	link = SortListInsert(link, 2)
+	PrintList(link)
+}
