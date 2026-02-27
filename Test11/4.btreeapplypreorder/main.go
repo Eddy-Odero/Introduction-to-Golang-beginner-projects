c44144e80169811ae18f527546b4fbbfacae08e5
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-02-27 18:52:10 +0300

    changes

---
+++ Test11/4.btreeapplypreorder/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                 string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+if root == nil {
+	return &TreeNode{Data:data}
+}
+if data < root.Data{
+	leftchild := BTreeInsertData(root.Left, data)
+	root.Left = leftchild
+	leftchild.Parent = root
+}else if data > root.Data{
+	rightchild := BTreeInsertData(root.Right,data)
+	root.Right = rightchild
+	rightchild.Parent = root
+}
+return root
+}
+func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
+if root == nil {
+return
+}
+BTreeApplyPreorder(root.Left,f)
+BTreeApplyPreorder(root.Right,f)
+}
+func main() {
+	root := &TreeNode{Data: "4"}
+	BTreeInsertData(root, "1")
+	BTreeInsertData(root, "7")
+	BTreeInsertData(root, "5")
+	BTreeApplyPreorder(root, fmt.Println)
+}
