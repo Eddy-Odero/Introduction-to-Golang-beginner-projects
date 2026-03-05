651bb481e9da57d52c2f666a50192b115d25a16b
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-03-05 22:09:16 +0300

    changes

--- Test11/checkpoint_exam5/3.cameltosnakecase/main.go
+++ Test11/checkpoint_exam5/3.cameltosnakecase/main.go
@@ -1,1 +1,45 @@
-.
+package main
+
+import (
+	"fmt"
+)
+func CamelToSnakeCase(s string) string{
+if s == ""{
+	return ""
+}
+for i,r := range s{
+	if (r < 'A' || r > 'Z') && (r < 'a' || r > 'z'){
+		return s
+	}
+	if i > 0 && r >= 'A'&& r <= 'Z' && s[i-1] >= 'A' && s[i-1] <= 'Z'{
+		return s
+	}
+}
+last := s[len(s)-1]
+
+	if last >= 'A' && last <= 'Z'{
+		
+      return s
+		
+	}
+	result := ""
+	for i,r := range s{
+	if r >= 'A' && r <= 'Z' && i != 0{
+		
+			result +="_"
+		}
+result +=string(r)
+	
+	}
+	return result
+}
+
+
+func main() {
+	fmt.Println(CamelToSnakeCase("HelloWorld"))
+	fmt.Println(CamelToSnakeCase("helloWorld"))
+	fmt.Println(CamelToSnakeCase("camelCase"))
+	fmt.Println(CamelToSnakeCase("CAMELtoSnackCASE"))
+	fmt.Println(CamelToSnakeCase("camelToSnakeCase"))
+	fmt.Println(CamelToSnakeCase("hey2"))
+}
