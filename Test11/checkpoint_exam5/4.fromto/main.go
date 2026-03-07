e824710eb0c1144f4e7b44ba8e3114cad5666cc5
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-03-07 21:33:48 +0300

    changes

--- Test11/checkpoint_exam5/4.fromto/main.go
+++ Test11/checkpoint_exam5/4.fromto/main.go
@@ -8,27 +8,20 @@ func FromTo(from int, to int) string {
 	if from < 0 || from > 99 || to < 0 || to > 99 {
 		return "Invalid\n"
 	}
-
 	result := ""
-
 	step := 1
 	if from > to {
 		step = -1
 	}
-
 	for i := from; ; i += step {
 		result += string('0'+(i/10)) + string('0'+(i%10))
-
 		if i == to {
 			break
 		}
-
 		result += ", "
 	}
-
 	return result + "\n"
 }
-
 func main() {
 	fmt.Print(FromTo(1, 10))
 	fmt.Print(FromTo(10, 1))
