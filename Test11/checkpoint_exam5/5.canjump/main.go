fa6bbe300b497a9e24da9aba706d63ee27ff0b10
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-03-08 20:01:56 +0300

    changes

--- Test11/checkpoint_exam5/5.canjump/main.go
+++ Test11/checkpoint_exam5/5.canjump/main.go
@@ -1,1 +1,35 @@
-.
+package main
+
+import (
+	"fmt"
+	
+)
+func CanJump(a []uint) bool {
+	if len(a) == 0 {
+		return false
+	}
+
+	i := 0
+	last := len(a) - 1
+
+	for i < len(a) {
+		if i == last {
+			return true
+		}
+		if a[i] == 0 || i+int(a[i]) > last {
+			return false
+		}
+		i += int(a[i])
+	}
+	return false
+}
+func main() {
+	input1 := []uint{2, 3, 1, 1, 4}
+	fmt.Println(CanJump(input1))
+
+	input2 := []uint{3, 2, 1, 0, 4}
+	fmt.Println(CanJump(input2))
+
+	input3 := []uint{0}
+	fmt.Println(CanJump(input3))
+}
