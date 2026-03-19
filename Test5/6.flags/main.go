b35805916f21db35ba5718ab2b087e2821143510
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-03-19 21:48:21 +0300

    updates

--- Test5/6.flags/main.go
+++ Test5/6.flags/main.go
@@ -6,60 +6,63 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintHelp() {
-	text := []string{
-		"--insert",
-		"  -i",
-		"--order",
-		" -o",
-	}
-	for _, line := range text {
-		for _, c := range line {
-			z01.PrintRune(c)
-		}
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
 	}
 }
 
+func printHelp() {
+	printStr("--insert\n  -i\n\t This flag inserts the string into the string passed as argument.\n")
+	printStr("--order\n  -o\n\t This flag will behave like a boolean, if it is called it will order the argument.\n")
+}
+
 func main() {
-	if len(os.Args) == 1 {
-		PrintHelp()
+	args := os.Args[1:]
+	if len(args) == 0 {
+		printHelp()
 		return
 	}
-	insert := ""
-	order := false
-	arg := ""
-	for _, a := range os.Args[1:] {
-		if a == "-h" || a == "--help" {
-			PrintHelp()
+	var insertStr string
+	var mainStr string
+	var orderFlag bool
+
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+
+		if arg == "--help" || arg == "-h" {
+			printHelp()
 			return
+		} else if len(arg) >= 9 && arg[:9] == "--insert=" {
+			insertStr = arg[9:]
+		} else if len(arg) >= 3 && arg[:3] == "-i=" {
+			insertStr = arg[3:]
+		} else if arg == "--insert" || arg == "-i" {
+			if i+1 < len(args) {
+				insertStr = args[i+1]
+				i++
+			}
+		} else if arg == "--order" || arg == "-o" {
+			orderFlag = true
+		} else {
+			mainStr = arg
 		}
-		if len(a) >= 9 && a[:9] == "--insert=" {
-			insert = a[9:]
-			continue
-		}
-		if a == "--order" || a == "-o" {
-			order = true
-			continue
-		}
-		arg = a
 	}
-	result := arg
-	if insert != "" {
-		result = result + insert
-	}
-	if order {
-		r := []rune(result)
-		for i := 0; i < len(r); i++ {
-			for j := i + 1; j < len(r); j++ {
-				if r[i] > r[j] {
-					r[i], r[j] = r[j], r[i]
+	combined := mainStr + insertStr
+	res := []rune(combined)
+	if orderFlag {
+		for i := 0; i < len(res)-1; i++ {
+			for j := 0; j < len(res)-i-1; j++ {
+				if res[j] > res[j+1] {
+					res[j], res[j+1] = res[j+1], res[j]
 				}
 			}
 		}
-		result = string(r)
 	}
-	for _, c := range result {
-		z01.PrintRune(c)
+	for _, r := range res {
+		z01.PrintRune(r)
 	}
-	z01.PrintRune('\n')
+	if len(res) > 0 {
+		z01.PrintRune('\n')
+	}
 }
