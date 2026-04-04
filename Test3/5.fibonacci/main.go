1bcc0510f1237729316b866a995138cd07f02254
Eddy-Odero <odiwuorodero8948@gmail.com>
2026-04-04 20:28:00 +0300

    changes

--- Test3/5.fibonacci/main.go
+++ Test3/5.fibonacci/main.go
@@ -1,5 +1,7 @@
  package main
+
  import "fmt"
+
  func Fibonacci(index int) int {
 if index < 0 {
 	return -1
@@ -12,6 +14,7 @@ if index == 1 {
 }
 return Fibonacci(index-1)+ Fibonacci(index-2)
 }
+
 func main(){
 	arg1 := 4
 	fmt.Println(Fibonacci(arg1))
